992a0e988b63844f2ab95c6babd36acf8c9e3966
Antoine Motet <antoine.motet@gmail.com>
2017-08-08 10:13:56 +0200

    base

--- scripts/ci_turbo.sh
+++ scripts/ci_turbo.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-git remote set-branches origin dev
-git fetch origin dev
+default_branch=foobar
+
+git remote set-branches origin $default_branch
+git fetch origin $default_branch
 
 if [[ ! -z "$TRAVIS_TAG" ]]
 then
     exit 0
 fi
 
-changed_files=$(git --no-pager diff --name-only $TRAVIS_COMMIT $(git merge-base $TRAVIS_COMMIT origin/dev))
+changed_files=$(git --no-pager diff --name-only $TRAVIS_COMMIT $(git merge-base $TRAVIS_COMMIT origin/$default_branch))
 
 if [[ "$ZDS_TEST_JOB" != "front" ]] && ! echo "$changed_files" | egrep -v "^assets"
 then
